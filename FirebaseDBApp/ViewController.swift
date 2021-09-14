232c7b93697bf6be72b0bc7c943da9fc3ac57450
DavidAML3 <David.aml.3@hotmail.com>
2021-09-13 21:28:34 -0500

    Added new logic for firebase work and complete app

--- FirebaseDBApp/ViewController.swift
+++ FirebaseDBApp/ViewController.swift
@@ -5,13 +5,52 @@
 //  Created by David Andres Mejia Lopez on 13/09/21.
 //
 
+import FirebaseDatabase
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let database = Database.database().reference()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        database.getData { _, snapshot in
+            if snapshot.exists() {
+                print("Got data \(snapshot.value!)")
+            } else {
+                print("No data available")
+            }
+        }
+        
+        database.childByAutoId().observeSingleEvent(of: .value, with: { snapshot in
+            guard let value = snapshot.value as? [String: Any] else {
+                return
+            }
+            
+            print("Value: \(value)")
+        })
+        
+        let button = UIButton(frame: CGRect(x: 20, y: 200, width: view.frame.size.width-40, height: 50))
+        button.setTitle("Add Entry", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .link
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(addNewEntry), for: .touchUpInside)
+    }
+    
+    @objc private func addNewEntry() {
+        let object: [String: Any] = [
+            "name": "iOS Academy",
+            "Youtube": "yes",
+            "working": true
+        ]
+        
+        database.observe(.childAdded) { (snapshot) in
+            print(snapshot.value!)
+        }
+        
+        database.childByAutoId().setValue(object)
     }
 
 
---
+++ Pods/Target Support Files/Pods-FirebaseDBApp-FirebaseDBAppUITests/Pods-FirebaseDBApp-FirebaseDBAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FirebaseDBApp_FirebaseDBAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FirebaseDBApp_FirebaseDBAppUITests
+@end
